6966745bb797ce6227c585aa6e21bb7e82d7766d
[roosey00] <[roosey79@gmail.com]>
2025-06-09 06:15:47 +0900

    인게임 UI추가
    플레이어 관련 UI 시스템을 추가할필요있음

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using System;
+using TMPro;
 
-public class GameManager : MonoBehaviourPunCallbacks
+
+
+public class GameManager : MonoBehaviour
 {
+    public int stage;
+    public float playTime;
+    public bool isBattle;
+    public TMP_Text stageTxt;
+    public TMP_Text playTimeTxt;
+    public TMP_Text playerHealthTxt;
+    public TMP_Text playerAmmoTxt;
+    public TMP_Text playerNameTxt;
     [SerializeField, ReadOnly(true)] private Transform[] spawnPoints;
-    
+
     [SerializeField, ReadOnly] private static GameObject player;
     public static GameObject Player => player;
     [SerializeField, ReadOnly] private static GameObject[] enemy;
     public static GameObject[] Enemy;
-
     private bool hasSpawned = false;
 
     void Start()
@@ -18,30 +31,38 @@ public class GameManager : MonoBehaviourPunCallbacks
         TrySpawn();
     }
 
-    public override void OnJoinedRoom()
-    {
-        base.OnJoinedRoom();
-        TrySpawn();
-    }
 
 
     private void TrySpawn()
     {
-        if (hasSpawned) return; // Áßº¹ ¹æÁö
+        if (hasSpawned) return; // ï¿½ßºï¿½ ï¿½ï¿½ï¿½ï¿½
         if (!PhotonNetwork.InRoom) return;
-        
+
         if (spawnPoints.Length == 0)
         {
-            Debug.LogWarning("spawnPoints°¡ ºñ¾î ÀÖ½À´Ï´Ù.");
+            Debug.LogWarning("spawnPointsï¿½ï¿½ ï¿½ï¿½ï¿½ ï¿½Ö½ï¿½ï¿½Ï´ï¿½.");
             return;
         }
 
-        int randomIndex = Random.Range(0, spawnPoints.Length);
-        Vector3 spawnPos = spawnPoints[randomIndex].position;
+        //int randomIndex = Random.Range(0, spawnPoints.Length);
+        //Vector3 spawnPos = spawnPoints[randomIndex].position;
 
-        player = PhotonNetwork.Instantiate("Prefabs/Player", spawnPos, Quaternion.identity);
+       //player = PhotonNetwork.Instantiate("Prefabs/Player", spawnPos, Quaternion.identity);
         hasSpawned = true;
 
-        Debug.Log("ÇÃ·¹ÀÌ¾î »ý¼º ¿Ï·á: " + player.name);
+        Debug.Log("ï¿½Ã·ï¿½ï¿½Ì¾ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½Ï·ï¿½: " + player.name);
+    }
+    void Update()
+    {
+        if (isBattle)
+            playTime += Time.deltaTime;
+    }
+    void LateUpdate()
+    {
+        int hour = (int)(playTime / 3600);
+        int min = (int)((playTime - hour * 3600) / 60);
+        int second = (int)(playTime % 60);
+        playTimeTxt.text = string.Format("{0:00}", hour) + ":" + string.Format("{0:00}", min) + ":" + string.Format("{0:00}", second);
+        stageTxt.text = "STAGE" + stage;
     }
 }
