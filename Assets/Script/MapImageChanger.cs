f9cc6c123dc70ea823ba0a377efe8f0996b0587d
Survivur <pgg071234@gmail.com>
2025-06-09 21:51:11 +0900

    정비됨

---
+++ Assets/Script/MapImageChanger.cs
@@ -0,0 +1,27 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapImageChanger : MonoBehaviour
+{
+    UnityEngine.UI.Image image;
+    RoomManager roomManager;
+
+    int prevStageNum = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        image = GetComponent<UnityEngine.UI.Image>();
+        roomManager = GameObjectRegistry.GetOrRegister("RoomManager").GetComponent<RoomManager>();
+
+        prevStageNum = roomManager.StageNum;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (prevStageNum != roomManager.StageNum)
+            image.sprite = Resources.Load<Sprite>($"Sprite/Stage{roomManager.StageNum}Background"); //roomManager.
+    }
+}
