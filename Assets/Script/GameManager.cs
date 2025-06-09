d2fcd32b0c5bdccfaba2f1e26722f0d86346c8de
[roosey00] <[roosey79@gmail.com]>
2025-06-09 16:11:47 +0900

    무기와충돌시 무기이름 가져오도록 작동
    현재 smg,minigun,rocket,shotgun,knife,hammer 입력. 추가입력 원할시 weapon script enum에 기입하기

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -18,6 +18,8 @@ public class GameManager : MonoBehaviour
     public TMP_Text playerHealthTxt;
     public TMP_Text playerAmmoTxt;
     public TMP_Text playerNameTxt;
+    public TMP_Text playerWeaponTxt;
+    public string[] Weapon = new string[] { "Minigun", "Shotgun", "Rocket", "Knife", "Smg" };
     [SerializeField, ReadOnly(true)] private Transform[] spawnPoints;
 
     [SerializeField, ReadOnly] private static GameObject player;
@@ -25,7 +27,21 @@ public class GameManager : MonoBehaviour
     [SerializeField, ReadOnly] private static GameObject[] enemy;
     public static GameObject[] Enemy;
     private bool hasSpawned = false;
+private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Weapon weapon = GetComponent<Weapon>();
+            if (weapon != null)
+            {
+                // 획득한 무기 이름을 UI에 표시
+                playerWeaponTxt.text = weapon.weaponName;
+            }
 
+            // 무기를 획득했으므로 사라지도록 설정
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
         TrySpawn();
@@ -46,7 +62,7 @@ public class GameManager : MonoBehaviour
         //int randomIndex = Random.Range(0, spawnPoints.Length);
         //Vector3 spawnPos = spawnPoints[randomIndex].position;
 
-       //player = PhotonNetwork.Instantiate("Prefabs/Player", spawnPos, Quaternion.identity);
+        //player = PhotonNetwork.Instantiate("Prefabs/Player", spawnPos, Quaternion.identity);
         hasSpawned = true;
 
         Debug.Log("�÷��̾� ���� �Ϸ�: " + player.name);
@@ -63,5 +79,8 @@ public class GameManager : MonoBehaviour
         int second = (int)(playTime % 60);
         playTimeTxt.text = string.Format("{0:00}", hour) + ":" + string.Format("{0:00}", min) + ":" + string.Format("{0:00}", second);
         stageTxt.text = "STAGE" + stage;
+        
     }
+  
+
 }
